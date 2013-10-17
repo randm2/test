b5fa21b2d2370f0d31d955f569a6792afe8e5578
Marc <martouchette@gmail.com>
2013-10-16 22:16:05 -0700

    comments

--- HttpServer.py
+++ HttpServer.py
@@ -2,6 +2,9 @@
 Created on Oct 16, 2013
 
 @author: marc
+
+modified from Kurr/Rosse
+Python 3.3
 '''
 #import socket module
 from socket import *
@@ -10,11 +13,11 @@ serverPort = 12000
 serverName = gethostname()
 print(gethostname())
 #Prepare a sever socket
-#Fill in start
+
 serverSocket.bind((serverName,serverPort))
 serverSocket.listen(0)
 print ('The server is ready to receive')
-#Fill in end
+
 while True:
 #Establish the connection
     print ('Ready to serve...')
@@ -25,23 +28,22 @@ while True:
             filename = message.split()[1]
             f = open(filename[1:])
             outputdata = f.read()
-            #Fill in start #Fill in end
+            
             #Send one HTTP header line into socket
-            #Fill in start
+            
             connectionSocket.send(b'HTTP/1.0 200 OK\r\n')
             connectionSocket.send(b'Content-Type: text/html\r\n\r\n')
-            #Fill in end
+           
             #Send the content of the requested file to the client
             for i in range (0, len(outputdata)):
                 connectionSocket.send(bytes(outputdata[i], 'ASCII'))
             connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        #Fill in start
+        
         connectionSocket.send(b'404 Not Found')
-        #Fill in end
-        #Close client socket
-        #Fill in start
+        
+        
         connectionSocket.close()
-        #Fill in end 
+
 serverSocket.close() 
